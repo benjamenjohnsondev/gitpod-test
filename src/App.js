c754bea16a3189057439eadd8372760c3666e789
benjamenhogben <bhogben@gmail.com>
2019-05-14 19:13:42 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './Logo-transparent.svg';
+import reactLogo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Full website coming soon
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built with React <img src={reactLogo} alt="react-logo"/>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
