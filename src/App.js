b8f143b432ccb3cf4db3015d6cb6acce0cc42477
benjamenhogben <bhogben@gmail.com>
2019-05-14 21:51:22 +0000

    add cool console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './media/Logo-transparent.svg';
 import reactLogo from './media/logo.svg';
+import Console from './component/Console';
 import './App.css';
 
 function App() {
@@ -8,18 +9,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Full website coming soon
-        </p>
+        <Console/>
+      </header>
+      <footer className="App-footer">
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Built with React <img src={reactLogo} alt="react-logo"/>
+          Built with React<img src={reactLogo} alt="react-logo"/>
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ src/component/Console.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Console.css';
+
+class Console extends Component
+{
+  render() {
+    return (
+      <div className="console">
+        <p>
+          > Full website coming soon <span className="flashing">_</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Console;
