84427ef66ab552fc7afa004a5b791002940979fe
benjamenhogben <bhogben@gmail.com>
2019-05-15 22:51:33 +0000

    working console

--- src/component/Console.js
+++ src/component/Console.js
@@ -10,21 +10,27 @@ class Console extends Component
       data: [
         'hey', 'world'
       ],
-      formData: ""
+      currText: ""
     }
     this.handleChange = this.handleChange.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
     this.consoleLog = React.createRef();
   }
   handleChange(event) {
-    this.setState({formData: event.target.value});
+    this.setState({
+      currText: event.target.value}
+    );
   }
   onSubmit(event) {
     event.preventDefault();
-    let joined = this.state.data.concat(this.state.formData)
-    this.setState({
-      data: joined
-    })
+    let joined = this.state.data.concat(this.state.currText)
+    if (joined !== this.state.data) {
+      this.setState({
+        data: joined,
+        currText: ""
+      })
+    }
+    window.scrollTo(0,document.body.scrollHeight);
   }
   render() {
     return (
