eb60362d4a45c4197f5508bbc4142ed8532407ae
Ben Hogben <bhogben@gmail.com>
2019-09-25 13:14:36 +0100

    change logo filename

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../media/logo-edited.svg';
-// import reactLogo from '../media/logo.svg';
 import styles from '../css/App.module.css';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
