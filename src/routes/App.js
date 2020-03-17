60b0fd1b9512ee1f3b927b245ddaccfe881891f6
Ben Hogben <bhogben@gmail.com>
2020-03-17 12:31:27 +0000

    add actions to toolbar

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,71 +1,117 @@
-import React from 'react';
+import React, { PureComponent } from "react";
 import logo from '../media/logo-edited.svg';
-import styles from '../css/App.module.css';
+import componentStyles from '../css/App.module.css';
+import buttonStyles from "../css/ButtonStyles.module.css";
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import classNames from "classnames/bind";
-let cx = classNames.bind(styles);
+let cx = classNames.bind(componentStyles);
 
-function App() {
-  let closeButton = cx({
-    [`${styles.Button}`]: true,
-    [`${styles.Close}`]: true
-  });
-  let minimiseButton = cx({
-    [`${styles.Button}`]: true,
-    [`${styles.Minimise}`]: true
-  });
-  let fullscreenButton = cx({
-    [`${styles.Button}`]: true,
-    [`${styles.Fullscreen}`]: true
-  });
-  return (
-    <div className={styles.AppWrap}>
-      <Helmet>
-        <meta charSet="utf-8" />
-      </Helmet>
-      <header className={styles.AppHeader}>
-        <div className={styles.ConsoleHeader}>
-          <button className={closeButton}/>
-          <button className={minimiseButton}/>
-          <button arian className={fullscreenButton}/>
-        </div>
-        <div className={styles.Console}>
-          <h1>
-            <img
-              src={logo}
-              className={styles.AppLogo}
-              width="700"
-              height="176"
-              alt="Ben Hogben - Full Stack Web Developer"
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actionStyles: "restore"
+    };
+  }
+
+  consoleAction(action) {
+    if (
+      (action === "fullscreen" && this.state.actionStyles === "minimise") ||
+      (action === "fullscreen" && this.state.actionStyles === "fullscreen")
+    ) {
+      this.consoleAction("restore");
+      return;
+    }
+    this.setState({
+      actionStyles: action
+    });
+    return;
+  }
+
+  componentDidMount() {
+    this.consoleAction("restore");
+  }
+
+  buttonStyles = {
+    closeButton: cx({
+      [buttonStyles.Button]: true,
+      [buttonStyles.Close]: true
+    }),
+    minimiseButton: cx({
+      [buttonStyles.Button]: true,
+      [buttonStyles.Minimise]: true
+    }),
+    fullscreenButton: cx({
+      [buttonStyles.Button]: true,
+      [buttonStyles.Fullscreen]: true
+    })
+  };
+
+  render() {
+    return (
+      <div
+        className={[
+          componentStyles.AppWrap +
+            " " +
+            componentStyles[this.state.actionStyles]
+        ]}
+      >
+        <Helmet>
+          <meta charSet="utf-8" />
+        </Helmet>
+        <header className={componentStyles.AppHeader}>
+          <div className={componentStyles.ConsoleHeader}>
+            <button
+              className={this.buttonStyles.closeButton}
+              onClick={() => this.consoleAction("close")}
             />
-            <span className={styles.HideMe}>
-              Ben Hogben - Full Stack Web Developer
-            </span>
-          </h1>
-          <p>
-            check out the <Link to="/console">console</Link>
-          </p>
-          <p>
-            > full website coming soon
-            <span className={styles.flashing}>_</span>
-          </p>
-          {/*<p>
-            <Link to="/console">> Console</Link>
-          </p>*/}
-        </div>
-      </header>
-      <footer className={styles.AppFooter}>
-        {/* <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria="react"
-        /> */}
-      </footer>
-    </div>
-  );
+            <button
+              className={this.buttonStyles.minimiseButton}
+              onClick={() => this.consoleAction("minimise")}
+            />
+            <button
+              className={this.buttonStyles.fullscreenButton}
+              onClick={() => this.consoleAction("fullscreen")}
+            />
+          </div>
+          <div className={componentStyles.Console}>
+            <h1>
+              <img
+                src={logo}
+                className={componentStyles.AppLogo}
+                width="700"
+                height="176"
+                alt="Ben Hogben - Full Stack Web Developer"
+              />
+              <span className={componentStyles.HideMe}>
+                Ben Hogben - Full Stack Web Developer
+              </span>
+            </h1>
+            <p>
+              check out the <Link to="/console">console</Link>
+            </p>
+            <p>
+              > full website coming soon
+              <span className={componentStyles.flashing}>_</span>
+            </p>
+            {/*<p>
+              <Link to="/console">> Console</Link>
+            </p>*/}
+          </div>
+        </header>
+        <footer className={componentStyles.AppFooter}>
+          {/* <a
+            className={styles.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria="react"
+          /> */}
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
